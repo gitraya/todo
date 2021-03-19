baf8d50b73acde3b6502e5ddca06d0087fe9484e
gitraya <mail.raya15@gmail.com>
2021-03-19 16:18:10 +0700

    Update Final of final

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 const DATA = [
   { id: 'todo-0', name: 'Eat', completed: true },
   { id: 'todo-1', name: 'Sleep', completed: false },
-  { id: 'todo-2', name: 'Repeat', completed: false },
+  { id: 'todo-2', name: 'Repeat', completed: true },
 ];
 
 ReactDOM.render(
